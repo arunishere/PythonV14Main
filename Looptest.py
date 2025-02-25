b750e009e83fdfa44964884fdaef66c0d1863ca4
arunmathic <arun@variable.net.in>
2025-02-25 23:50:05 +0530

    Updated

---
+++ Looptest.py
@@ -0,0 +1,25 @@
+
+
+
+#acid. 
+
+# A -- > B --- > C --- > D
+
+#php, java, python
+
+#sql command
+
+balance = 20000
+customer_name = "Ramesh"
+account_num = 909090900
+type_trax = "Credited"
+credited_amt = 10000
+total_bal = balance + credited_amt
+
+
+message_1 = f"""Dear {customer_name}, your account number ending with {account_num} 
+            has been {type_trax} with {credited_amt},
+            total bal is {total_bal}. if this not done by you reach out to @email.com"""
+
+
+print(message_1)
---
+++ WhileLoop.py
@@ -0,0 +1,49 @@
+#Repeting -> doing the same action multipe 
+#Until some codition is met. 
+
+#Loops
+
+#While Loop
+
+
+
+# a = 1
+
+# while a < 5:
+
+#     print("Good Evening!")
+
+#     a = a + 1
+
+
+name = "Vivek"
+
+location = "Hyderabad"
+
+
+# print("Good Evening", name, "How are you doing today")
+
+#f -string approach
+
+# print(f"Good Evening {name}. How are doing today")
+
+
+
+"Dear name, your account number ending with xxx has been credited/debited with xxx, total bal is xxx. if this not done by you reach out to @email.com"
+
+
+#data from SQL
+
+balance = 20000
+customer_name = "Ramesh"
+account_num = 909090900
+type_trax = "Credited"
+credited_amt = 10000
+total_bal = balance + credited_amt
+
+
+print(f"Dear {customer_name}, your account number ending with {account_num} has been {type_trax} with {credited_amt}, total bal is {total_bal}. if this not done by you reach out to @email.com")
+
+
+
+# print("Good Evening {}. How are you doing today, How is the weather in {}".format(name, location))
